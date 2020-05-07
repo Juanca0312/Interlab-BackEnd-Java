f59840259e01e3fa40508399881d61ad904145d6
sadaakisz <hssm2000.sadaaki@gmail.com>
2020-05-07 15:17:58 -0500

    Added ModelMapper dependency.

--- src/main/java/com/acme/interlab/InterlabApplication.java
+++ src/main/java/com/acme/interlab/InterlabApplication.java
@@ -1,8 +1,11 @@
 package com.acme.interlab;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.ui.Model;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -12,4 +15,6 @@ public class InterlabApplication {
         SpringApplication.run(InterlabApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() { return new ModelMapper(); }
 }
