cea7b11015c5114919941913a6fafc1534a8875f
Omar Alonso Clavijo Mestas <u201817916@upc.edu.pe>
2020-05-07 11:33:14 -0500

    creado la clase AuditModel

---
+++ src/main/java/com/acme/interlab/Model/AuditModel.java
@@ -0,0 +1,32 @@
+package com.acme.interlab.Model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"CreatedAt", "UpdateAt"}, allowGetters = true)
+
+public abstract class AuditModel implements Serializable {
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date CreatedAt;
+
+
+    @Column(nullable = false )
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date UpdateAt;
+
+}
