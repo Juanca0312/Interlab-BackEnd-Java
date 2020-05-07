374e9f8616ac92705862084e261a2395fd85f34f
Juanca0312 <39997847+Juanca0312@users.noreply.github.com>
2020-05-07 13:16:25 -0500

    Student Model

--- src/main/java/com/acme/interlab/Model/Student.java
+++ src/main/java/com/acme/interlab/Model/Student.java
@@ -1,6 +1,24 @@
 package com.acme.interlab.Model;
 
-public class Student {
-    String name;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "students")
+public class Student extends AuditModel{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true)
+    @NotBlank
+    @NotNull
+    @Size(max = 50 )
+    private String university;
+
+    //Relationships
+
 
 }
