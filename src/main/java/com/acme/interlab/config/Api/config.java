dc1be178f49b12334948388af1b6c12705da7553
Omar Alonso Clavijo Mestas <u201817916@upc.edu.pe>
2020-05-08 18:49:21 -0500

    Edition the class config

--- src/main/java/com/acme/interlab/config/Api/config.java
+++ src/main/java/com/acme/interlab/config/Api/config.java
@@ -1,4 +1,25 @@
 package com.acme.interlab.config.Api;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import lombok.ToString;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@ToString
+@Configuration
 public class config {
+
+@Bean(name = "interlabOpenApi")
+    public OpenAPI interlabOpenAPI(){
+    //Available at
+
+    return new OpenAPI()
+            .components(new Components())
+            .info(new Info().title("InterLab Application API").description(
+                    "InterLab API implemented with Spring Boot Restful service and documented using springdoc-openapi and OpenApi 3. "
+            ));
+    }
 }
+
